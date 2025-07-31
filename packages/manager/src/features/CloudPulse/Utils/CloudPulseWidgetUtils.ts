313293d8e39d1c3b14f375705352e05e603d918d
nikhagra-akamai <nagrawal@akamai.com>
2025-07-31 17:38:07 +0530

    change: [DI-26544] - Update cloud pulse metric request to handle invalid preset

--- packages/manager/src/features/CloudPulse/Utils/CloudPulseWidgetUtils.ts
+++ packages/manager/src/features/CloudPulse/Utils/CloudPulseWidgetUtils.ts
@@ -267,18 +267,18 @@ export const getCloudPulseMetricRequest = (
 ): CloudPulseMetricsRequest => {
   const { duration, entityIds, resources, widget } = props;
   const preset = duration.preset;
-
+  const presetDuration = getTimeDurationFromPreset(preset);
   return {
     absolute_time_duration:
-      preset !== 'reset' && preset !== 'this month' && preset !== 'last month'
-        ? undefined
-        : { end: duration.end, start: duration.start },
+      presetDuration === undefined
+        ? { end: duration.end, start: duration.start }
+        : undefined,
     entity_ids: resources
       ? entityIds.map((id) => parseInt(id, 10))
       : widget.entity_ids.map((id) => parseInt(id, 10)),
     filters: undefined,
     group_by: widget.group_by,
-    relative_time_duration: getTimeDurationFromPreset(preset),
+    relative_time_duration: presetDuration,
     metrics: [
       {
         aggregate_function: widget.aggregate_function,
