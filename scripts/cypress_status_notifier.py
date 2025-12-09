df2cfcce6cb556572dfe3a840bfd21bbbf6954e2
venkatmano-akamai <vmangalr@akamai.com>
2025-12-09 18:21:25 +0530

    Add Cypress status notifier script

---
+++ scripts/cypress_status_notifier.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from random import randint
+
+
+
+messages = [
+"""> 🛠️ **Cypress Run Completed**  
+> **Title:** _{TITLE}_  
+> 👤 Author: `{AUTHOR}`  
+> 🔗 [View PR]({URL})  
+>  **Job Status:** _{STATUS}_
+>  **Run Url:** _{RUN_URL}_
+> Dev team, jump in for review when you can!
+""",]
+
+
+def send_message():
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    ROOM_ID = os.getenv('ROOM_ID')
+    URL = os.getenv('url')
+    TITLE = os.getenv('title')
+    AUTHOR = os.getenv('author')
+    STATUS = os.getenv('status')
+    RUN_URL = os.getenv('run_url')
+
+    url = 'https://webexapis.com/v1/messages'
+    headers = {
+        'Authorization': f'Bearer {BOT_TOKEN}',
+        'Content-Type': 'application/json'
+    }
+    data = {        
+        'roomId': ROOM_ID,
+        'markdown': getRandomMessage().format(TITLE=TITLE, AUTHOR=AUTHOR, URL=URL, STATUS=STATUS),
+    }
+
+    requests.post(url, headers=headers, json=data)
+
+def getRandomMessage():
+
+    return messages[randint(0, len(messages) - 1)]
+
+send_message()
