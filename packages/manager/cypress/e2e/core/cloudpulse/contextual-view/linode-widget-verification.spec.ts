0b6b7c5ac6c65d71dee14c47b92726bd1979f3e3
agorthi-akamai <agorthi@akamai.com>
2025-12-15 11:16:18 +0530

    test:range picker fixes

--- packages/manager/cypress/e2e/core/cloudpulse/contextual-view/linode-widget-verification.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/contextual-view/linode-widget-verification.spec.ts
@@ -18,6 +18,7 @@ import {
   mockGetLinodes,
   mockGetLinodeStats,
 } from 'support/intercepts/linodes';
+import { mockGetUserPreferences } from 'support/intercepts/profile';
 import { mockGetRegions } from 'support/intercepts/regions';
 import { ui } from 'support/ui';
 import {
@@ -134,6 +135,7 @@ describe('Integration Tests for Linode Dashboard ', () => {
     mockGetCloudPulseDashboard(id, dashboard);
     mockCreateCloudPulseJWEToken(serviceType);
     mockGetRegions([mockRegion]);
+    mockGetUserPreferences({ isAclpMetricsBeta: true }).as('fetchPreferences');
     mockCreateCloudPulseMetrics(serviceType, metricsAPIResponsePayload).as(
       'getMetrics'
     );
@@ -143,6 +145,7 @@ describe('Integration Tests for Linode Dashboard ', () => {
 
     // navigate to the metrics page
     cy.visitWithLogin(`/linodes/${mockLinode.id}/metrics`);
+    cy.wait('@fetchPreferences');
 
     cy.get('button[data-testid="button"]').then(($btns) => {
       const match = $btns
@@ -155,8 +158,11 @@ describe('Integration Tests for Linode Dashboard ', () => {
       }
     });
     cy.get('[role="tablist"]').within(() => {
-      cy.get('[data-testid="Metrics"]')
-        .should('have.attr', 'aria-label', 'Metrics ')
+      cy.get('[data-testid="Metrics"]').should(
+        'have.attr',
+        'aria-label',
+        'Metrics '
+      );
     });
 
     cy.wait('@fetchDashboard');
