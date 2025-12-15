c46e4dd01b547cce6bd88f38ff8275cc8a20cb85
ankitaakamai <ankitaan@akamai.com>
2025-12-15 14:17:14 +0530

    upcoming: [DI-28503] - Make flag optional

--- packages/manager/src/featureFlags.ts
+++ packages/manager/src/featureFlags.ts
@@ -151,7 +151,7 @@ interface AclpAlerting {
   alertDefinitions: boolean;
   beta: boolean;
   editDisabledStatuses?: AlertStatusType[];
-  maxEmailRecipients: number;
+  maxEmailRecipients?: number;
   notificationChannels: boolean;
   recentActivity: boolean;
   systemChannelSupportedServices?: CloudPulseServiceType[]; //linode, dbaas, etc. 
