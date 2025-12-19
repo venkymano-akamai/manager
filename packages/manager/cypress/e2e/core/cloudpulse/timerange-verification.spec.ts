4df610a7cbf85413bbfd9105a430bd2d00554f24
agorthi-akamai <agorthi@akamai.com>
2025-12-19 10:52:57 +0530

    fixing failed spec

--- packages/manager/cypress/e2e/core/cloudpulse/timerange-verification.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/timerange-verification.spec.ts
@@ -268,9 +268,10 @@ describe('Integration tests for verifying Cloudpulse custom and preset configura
     cy.get('@startDateInput').scrollIntoView();
 
     cy.get('@startDateInput').click();
+
     cy.get('[role="dialog"]').within(() => {
       cy.findAllByText(startDay).first().click();
-      //cy.findAllByText(endDay).first().click();
+      cy.findAllByText(endDay).first().click();
     });
 
     ui.button
