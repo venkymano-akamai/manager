9b98a6b9572145989a5b411706e41f63870f3e3f
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2025-12-22 13:09:06 +0530

    fix duplicate route

--- packages/manager/src/routes/objectStorage/index.ts
+++ packages/manager/src/routes/objectStorage/index.ts
@@ -98,15 +98,6 @@ const objectStorageBucketDetailAccessRoute = createRoute({
   ).then((m) => m.bucketDetailLandingLazyRoute)
 );
 
-const objectStorageBucketMetricsRoute = createRoute({
-  getParentRoute: () => objectStorageBucketDetailRoute,
-  path: 'metrics',
-}).lazy(() =>
-  import(
-    'src/features/ObjectStorage/BucketDetail/bucketDetailLandingLazyRoute'
-  ).then((m) => m.bucketDetailLandingLazyRoute)
-);
-
 const objectStorageBucketSSLRoute = createRoute({
   getParentRoute: () => objectStorageBucketDetailRoute,
   path: 'ssl',
