13990962552992f6a69f7d5aef7fbcbef2a8a0fa
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2026-01-05 16:53:08 +0530

    simplify details checking logic

--- packages/manager/src/features/CloudPulse/Alerts/Utils/utils.ts
+++ packages/manager/src/features/CloudPulse/Alerts/Utils/utils.ts
@@ -243,16 +243,10 @@ export const getAlertChipBorderRadius = (
 export const shouldUseContentsForEmail = (
   channel: NotificationChannel
 ): boolean => {
-  const detailsEmail =
-    channel.channel_type === 'email' ? channel.details?.email : undefined;
   // Use content if: details is missing, details is empty, details.email is empty or details.email.usernames is empty
-  return (
-    !channel.details ||
-    Object.keys(channel.details).length === 0 ||
-    !detailsEmail ||
-    Object.keys(detailsEmail).length === 0 ||
-    !detailsEmail.usernames ||
-    detailsEmail.usernames.length === 0
+  return !(
+    channel.channel_type === 'email' && // ensuring it's an email channel to avoid the type error with email property
+    channel.details?.email?.usernames?.length
   );
 };
 
