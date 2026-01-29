7de52eeb012277597bf5934835e0d19e9eef98c3
santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
2026-01-29 14:44:53 +0530

    revert mocks to normal behaviour

--- packages/manager/src/mocks/serverHandlers.ts
+++ packages/manager/src/mocks/serverHandlers.ts
@@ -3760,15 +3760,7 @@ export const handlers = [
     return HttpResponse.json(makeResourcePage(notificationChannels));
   }),
   http.post('*/monitor/alert-channels', () => {
-    // return HttpResponse.json(notificationChannelFactory.build());
-    //temporarily return an error to verify the changes
-    return HttpResponse.json(
-      {
-        errors: [{ field: 'label', reason: 'Label already exists' }],
-      },
-
-      { status: 400 }
-    );
+    return HttpResponse.json(notificationChannelFactory.build());
   }),
   http.put('*/monitor/alert-channels/:id', () => {
     return HttpResponse.json(notificationChannelFactory.build());
