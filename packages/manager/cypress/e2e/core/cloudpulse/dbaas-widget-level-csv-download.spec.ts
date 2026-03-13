581122b4fe2772bd5d6cff2544d2c3b0db2d800f
agorthi-akamai <agorthi@akamai.com>
2026-03-13 12:02:43 +0530

    fixing timezone issue

--- packages/manager/cypress/e2e/core/cloudpulse/dbaas-widget-level-csv-download.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/dbaas-widget-level-csv-download.spec.ts
@@ -153,11 +153,15 @@ const validateCSV = (
     if (widgetConfig.dateSelection === 'Reset') {
       const csvStartTime = getValue(lines, 'Start Time');
       expect(csvStartTime.key).to.equal('Start Time');
-      expect(csvStartTime.value).to.equal(widgetConfig.startDate);
-
+    
+      expect(new Date(csvStartTime.value).getTime())
+        .to.equal(new Date(widgetConfig.startDate).getTime());
+    
       const csvEndTime = getValue(lines, 'End Time');
       expect(csvEndTime.key).to.equal('End Time');
-      expect(csvEndTime.value).to.equal(widgetConfig.endDate);
+    
+      expect(new Date(csvEndTime.value).getTime())
+        .to.equal(new Date(widgetConfig.endDate).getTime());
     } else {
       const csvDuration = getValue(lines, 'Time Range');
       expect(csvDuration.key).to.equal('Time Range');
